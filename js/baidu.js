80c7da017665be9a5755e5e03f4716ad1dd388f3
pdone <617941447@qq.com>
2020-09-17 09:12:18 +0800

    add layui resources

---
+++ js/baidu.js
@@ -0,0 +1,10 @@
+/*
+百度统计
+*/
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1a864f4af9f5edebadeb6044213eb4aa";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
